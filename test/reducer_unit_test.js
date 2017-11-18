fad5432f3308dd7a247720627eefd42ae531a58e
Patrick King <patrick.king@vizworx.com>
2017-11-17 17:15:02 -0700

    Add test tools, and some stub files to serve as example tests. Add package-lock.json.

---
+++ test/reducer_unit_test.js
@@ -0,0 +1,10 @@
+let assert = require('assert')
+
+describe('Arrayz', function() {
+  describe('#indexOf()z', function() {
+    it('should return -1 when the value is not present ...', function() {
+      assert.equal(3, [1,2,3, 4].indexOf(4))
+    })
+  })
+})
+
