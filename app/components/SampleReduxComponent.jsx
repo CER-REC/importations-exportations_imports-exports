28b83e074dd57c0a060731ab6edb0454fb2f592b
Patrick King <patrick.king@vizworx.com>
2017-11-20 16:51:42 -0700

    Sample tests using redux.

---
+++ app/components/SampleReduxComponent.jsx
@@ -0,0 +1,22 @@
+const React = require('react')
+const ReactRedux = require('react-redux')
+
+class SampleReduxComponent extends React.Component {
+
+  render() {
+    return <div>
+      <p>{ `Why hello there ${this.props.name}` }</p>
+    </div>
+  }
+
+}
+
+
+const mapStateToProps = state => {
+  return {
+    name: state.name,
+  }
+}
+
+module.exports = ReactRedux.connect(mapStateToProps)(SampleReduxComponent)
+module.exports.unwrapped = SampleReduxComponent
