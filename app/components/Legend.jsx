fa4f9cb4b0fa239e94d203bbd088a9c25118629c
ccheuvizworx <30633884+ccheuvizworx@users.noreply.github.com>
2017-12-14 17:14:59 -0700

    legend component with placeholder images

--- app/components/Legend.jsx
+++ app/components/Legend.jsx
@@ -4,19 +4,198 @@ const ReactRedux = require('react-redux')
 const Constants = require('../Constants.js')
 const WorkspaceComputations = require('../computations/WorkspaceComputations.js')
 
+require('../styles/Fonts.scss')
+
 require('./Legend.scss')
 
 class Legend extends React.Component {
 
+  importColumn() {
+    return <svg
+      className='importColumn'
+    >
+      <g>
+        <rect
+          y = {850}
+          x={5}
+          width = {50}
+          height = { Constants.getIn(['legend','height']) }
+          fill = "#fff"
+        />
+
+        <g>
+          <text className='theLegendHeading'
+            x = {15} 
+            y={870} 
+          > IMP
+          </text>
+        </g>
+
+        <g>
+          <image 
+            className = 'legendImage'
+            height = {22}
+            width = {35}
+            x = {12}
+            y = { 880 }
+            xlinkHref = 'images/close-2.svg'
+          ></image>
+          <image 
+            className = 'legendImage'
+            height = {22}
+            width = {35}
+            x = {12}
+            y = { 915 }
+            xlinkHref = 'images/close-2.svg'
+          ></image>
+          <image 
+            className = 'legendImage'
+            height = {22}
+            width = {35}
+            x = {12}
+            y = { 950 }
+            xlinkHref = 'images/close-2.svg'
+          ></image>
+          <image 
+            className = 'legendImage'
+            height = {22}
+            width = {35}
+            x = {12}
+            y = { 985 }
+            xlinkHref = 'images/close-2.svg'
+          ></image>
+          <image 
+            className = 'legendImage'
+            height = {22}
+            width = {35}
+            x = {12}
+            y = { 1020 }
+            xlinkHref = 'images/close-2.svg'
+          ></image>
+        </g>
+      </g>
+    </svg>
+  }
+
+  exportColumn() {
+    return <svg
+      className='exportColumn'
+    >
+      <g>
+        <rect
+          y = {850}
+          x={55}
+          width = {50}
+          height = { Constants.getIn(['legend','height']) }
+          fill = "#fff"
+        />
+
+        <g>
+          <text className='theLegendHeading'
+            x = {55} 
+            y={870} 
+          > EXP
+          </text>
+        </g>
+
+        <g>
+          <image 
+            className = 'legendImage'
+            height = {22}
+            width = {35}
+            x = {52}
+            y = { 880 }
+            xlinkHref = 'images/close-2.svg'
+          ></image>
+          <image 
+            className = 'legendImage'
+            height = {22}
+            width = {35}
+            x = {52}
+            y = { 915 }
+            xlinkHref = 'images/close-2.svg'
+          ></image>
+          <image 
+            className = 'legendImage'
+            height = {22}
+            width = {35}
+            x = {52}
+            y = { 950 }
+            xlinkHref = 'images/close-2.svg'
+          ></image>
+          <image 
+            className = 'legendImage'
+            height = {22}
+            width = {35}
+            x = {52}
+            y = { 985 }
+            xlinkHref = 'images/close-2.svg'
+          ></image>
+          <image 
+            className = 'legendImage'
+            height = {22}
+            width = {35}
+            x = {52}
+            y = { 1020 }
+            xlinkHref = 'images/close-2.svg'
+          ></image>
+        </g>
+
+      </g>
+    </svg>
+  }
+
+  textValues() {
+    return <svg> 
+      <g>
+
+        <rect
+          y = {850}
+          x={95}
+          width = {170}
+          height = { Constants.getIn(['legend','height']) }
+          fill = "#fff"
+        />
+
+        <g>
+          <text className='theLegendValues'
+            x = {88} 
+            y={897} 
+          > 1 - 9,9999
+          </text>
+          <text className='theLegendValues'
+            x = {88} 
+            y={932} 
+          > 10,000 - 99, 999
+          </text>
+          <text className='theLegendValues'
+            x = {88} 
+            y={967} 
+          > 100,000 - 999,999
+          </text>
+          <text className='theLegendValues'
+            x = {88} 
+            y={1002} 
+          > 1,000,000 - 1,999,999
+          </text>
+          <text className='theLegendValues'
+            x = {88} 
+            y={1037} 
+          > 2,000,000 >
+          </text>
+        </g>
+
+      </g>
+    </svg>
+  }
+
+
+
   render() {
     return <g>
-      <rect 
-        x = { 0 }
-        y = {WorkspaceComputations.legendY(this.props.viewport)}
-        width = { Constants.getIn(['legend','width']) }
-        height = { Constants.getIn(['legend','height']) }
-        fill = '#ebf2bc' 
-      />
+      {this.importColumn()}
+      {this.exportColumn()}
+      {this.textValues()}
     </g>
   }
 }
