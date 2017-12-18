962cade7d19c7d69f6eda26743fced71ba8ec2d0
Rahul Kamal Bhaskar <rahul.kamal.bhaskar@vizworx.com>
2017-12-18 16:05:58 -0700

    added hexagon svg using D#

--- app/components/MapLayout.jsx
+++ app/components/MapLayout.jsx
@@ -6,12 +6,16 @@ const DataLoader = require('../DataLoader.js')
 
 const MapLayoutGridConstant = require('../MapLayoutGridConstant.js')
 class MapLayout extends React.Component {
+  mapPieceTransform (xaxis, yaxis, position, dimensions){
+    let startXaxis = xaxis + (position.get('x') * (dimensions.get('width') + dimensions.get('xAxisPadding')))
+    let startYaxis = yaxis + (position.get('y') * (dimensions.get('height') + dimensions.get('yAxisPadding')))
+    return`translate(${startXaxis+','+startYaxis})`
+  }
   render(){
     //Data from constant file
     const type = this.props.importExportVisualization
     const mapData = DataLoader.get(type)
     
-
     //fetching nested values
     const mapLayoutGrid = MapLayoutGridConstant.getIn([type, this.props.country])
     const layout = mapLayoutGrid.get('layout')
@@ -19,16 +23,17 @@ class MapLayout extends React.Component {
     const styles = mapLayoutGrid.get('styles')
 
     const mapPieceData = mapData.get(this.props.country)
+    const xaxis = this.props.xaxis
+    const yaxis = this.props.yaxis
     
     return layout.map( (position,key) =>{
-      return <MapPiece 
-        key = { key } 
-        startCoordinateXaxis = { this.props.xaxis }
-        startCoordinateYaxis = { this.props.yaxis }
-        data = { mapPieceData.get(position.get('name')) } 
-        position = { position }
-        dimensions = { dimensions }
-        styles = { styles }/>
+      
+      return <g  key = { key }  transform={this.mapPieceTransform(xaxis, yaxis, position, dimensions)} >
+                <MapPiece 
+                data = { mapPieceData.get(position.get('name')) } 
+                dimensions = { dimensions }
+                styles = { styles }/>
+            </g>
     })
   }
 }
--- app/components/MapPiece.jsx
+++ app/components/MapPiece.jsx
@@ -1,27 +1,64 @@
 const React = require('react')
 const ReactRedux = require('react-redux')
-
+const ReactFauxDOM = require('react-faux-dom')
+const D3 = require('d3')
 class MapPiece extends React.Component {
-  render(){
-    //intial x and y coordinates
-    let startCoordinateXaxis = parseInt(this.props.startCoordinateXaxis)
-    let startCoordinateYaxis = parseInt(this.props.startCoordinateYaxis)
 
+  render(){
+    //Create the element
+    const g = new ReactFauxDOM.Element('g')
+    
+    //intial x and y coordinates
+    const initialX = 0
+    const intiailY = 0
+   
     //Map Piece width and height
+    //hexagon height : width = 9:10
     const width = parseInt(this.props.dimensions.get('width'))
     const height = parseInt(this.props.dimensions.get('height'))
 
-    //calculated x and y coordinate
-    const xaxis = startCoordinateXaxis + (this.props.position.get('x') * ( width + this.props.dimensions.get('xAxisPadding')))
-    const yaxis = startCoordinateYaxis + (this.props.position.get('y') * (height + this.props.dimensions.get('yAxisPadding')))
+    let lengthOfLine = Math.sqrt(((initialX -(initialX + width/2)) * (initialX -(initialX + width/2))) + 
+                          (((intiailY+height/2) - intiailY)*((intiailY+height/2) - intiailY)))
+    lengthOfLine = Math.round(lengthOfLine)
 
-    //Mock data need to be replaced by actual content 
-    return <g>
-      <rect x={ xaxis } y = { yaxis } width={width} height={height} fill = {this.props.styles.get('color')}></rect>
-      <text x={ xaxis + (width / 2 )  } y={ yaxis + (height / 2) } fill = "blue">{this.props.data.get('name')}</text>
-      <text x= { xaxis + (width / 3) } y= { yaxis + (height / 5) } fill = "blue">{this.props.data.get('importData')}</text>
-      <text x= { xaxis + (width / 3) } y= { yaxis + (height * 0.8) } fill = "blue">{this.props.data.get('exportData')}</text>
-    </g>
+    var hexagonData = [  { "x": initialX,   "y": intiailY+ height/4} ,  { "x": initialX + width/2,  "y": intiailY},
+                      { "x": initialX + width/2,  "y": intiailY}, { "x": initialX + width,  "y": intiailY+ height/4},
+                      { "x": initialX + width,  "y": intiailY+ height/4},  { "x": initialX + width,  "y": lengthOfLine},
+                      { "x": initialX + width,  "y": lengthOfLine},  { "x": initialX + width/2,  "y": intiailY+height},
+                      { "x": initialX + width/2,  "y": intiailY+height},  { "x": initialX,   "y": lengthOfLine} ,
+                      { "x": initialX,   "y": lengthOfLine}, { "x": initialX,   "y": intiailY+ height/4} 
+                    ]
+     var hexagon = D3.line()
+        .x(function(d) { return d.x; })
+        .y(function(d) { return d.y; })
+        .curve(D3.curveBasis)
+
+      D3.select(g).append("path")
+        .attr("d", hexagon(hexagonData))
+        .attr("fill", this.props.styles.get('color'))
+
+      //append province Label
+      D3.select(g).append("text")
+        .attr("dx", initialX+width/2)
+        .attr("dy", intiailY+height/2)
+        .text(this.props.data.get('name'))
+
+      //These will be replaced by the arrow polygon
+      //append import data
+       D3.select(g).append("text")
+        .attr("dx", initialX + (width / 3) )
+        .attr("dy", intiailY + (height / 5) )
+        .text(this.props.data.get('importData'))
+
+      //append export data
+      D3.select(g).append("text")
+        .attr("dx", initialX + (width / 3) )
+        .attr("dy", intiailY + (height * 0.8) )
+        .text(this.props.data.get('exportData'))
+
+    //DOM manipulations done, convert to React
+    return g.toReact()
+    
   }
 }
 
