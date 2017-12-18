509b14174a6e2a705fbb22c174304dce71705d96
Rahul Kamal Bhaskar <rahul.kamal.bhaskar@vizworx.com>
2017-12-18 11:15:36 -0700

    adjust spacing between elements inside in the alternative row

--- app/MapLayoutGridConstant.js
+++ app/MapLayoutGridConstant.js
@@ -18,14 +18,14 @@ const MapLayoutGridConstant = Immutable.fromJS({
         { name:'NU',  x:2,  y:0 },
         { name:'NF',  x:6,  y:0 },
         { name:'PE',  x:7,  y:0 },
-        { name:'BC',  x:0,  y:1 },
-        { name:'AB',  x:1,  y:1 },
-        { name:'SK',  x:2,  y:1 },
-        { name:'MB',  x:3,  y:1 },
-        { name:'ON',  x:4,  y:1 },
-        { name:'QC',  x:5,  y:1 },
-        { name:'NB',  x:6,  y:1 },
-        { name:'NS',  x:7,  y:1 } 
+        { name:'BC',  x:0.5,  y:1 },
+        { name:'AB',  x:1.5,  y:1 },
+        { name:'SK',  x:2.5,  y:1 },
+        { name:'MB',  x:3.5,  y:1 },
+        { name:'ON',  x:4.5,  y:1 },
+        { name:'QC',  x:5.5,  y:1 },
+        { name:'NB',  x:6.5,  y:1 },
+        { name:'NS',  x:7.5,  y:1 } 
       ]
     } 
   } 
