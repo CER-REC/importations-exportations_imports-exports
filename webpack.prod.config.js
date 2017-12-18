cf169057d68ed1975ad4075011a90a3a67075f1f
Spenser Jones <hello@spenserj.com>
2017-12-18 09:32:07 -0700

    NEBV-768 Clean up production output

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -13,5 +13,12 @@ config.plugins = [
 
 ]
 
+// Disable sourcemaps for production builds
+config.devtool = false
+// Remove react-hot-loader and webpack-hot-middleware
+config.entry.bundle = './app/App.jsx'
+config.output.publicPath = ''
+config.module.rules[0].use.options.plugins = []
+
 module.exports = config
 
