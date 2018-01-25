597296d23d56fc937428406025b08cd4edaf79d3
Rahul Kamal Bhaskar <rahul.kamal.bhaskar@vizworx.com>
2018-01-25 16:36:49 -0700

    added all the padds

---
+++ app/components/Padds/PaddThree.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import ImportExportArrow from '../ImportExportArrow.jsx'
+import Immutable from 'immutable'
+const PaddThree = props => (
+  <g>
+    <g transform="translate(0 50)">
+      <ImportExportArrow
+          arrowSpacing= {0}
+          type= 'exports'
+          color={props.color}
+          arrowProps={
+            Immutable.fromJS({
+              exports:{
+                bottomText: {
+                  en: "PADD3",
+                  fr: "PADD3",
+                }
+              }
+            })
+          }
+        />
+      </g>
+    <polygon 
+    transform="scale(0.84)"
+    fill ={props.color}
+    points="63.61 15.74 54.52 20.98 45.44 15.74 45.44 5.25 36.35 0 27.26 5.25 27.26 15.73 18.17 20.98 18.17 20.99 9.09 15.74 0 20.99 0 31.48 9.09 36.73 9.09 47.2 18.17 52.45 27.26 47.2 27.26 36.72 27.26 36.72 36.35 31.47 36.35 31.48 45.44 36.73 54.52 31.48 63.61 36.73 72.7 31.48 72.7 20.98 63.61 15.74"/>
+  </g>  
+)
+
+PaddThree.propTypes = {
+  color: PropTypes.string.isRequired,
+  arrowLabel: PropTypes.string.isRequired,
+}
+
+module.exports = PaddThree
--- app/components/Padds/USPadd.jsx
+++ app/components/Padds/USPadd.jsx
@@ -2,14 +2,36 @@ const React = require('react')
 const ReactRedux = require('react-redux')
 const PropTypes = require('prop-types')
 import PaddOne from './PaddOne'
+import PaddTwo from './PaddTwo'
+import PaddThree from './PaddThree'
+import PaddFour from './PaddFour'
+import PaddFive from './PaddFive'
 
+//fetch x axis of the padd using the left detail side bar location
 const USPadd = props => (
-	<g transform = {`translate(${props.top} ${props.left})`}>
-		<PaddOne color="red" arrowLabel="Padd1"/>
-	</g>
-)
-const mapStateToProps = state => ({
-  viewport: state.viewport,
-})
+{}
+	/*<g transform = {`scale(4) translate(${props.left/4} ${props.top/4})`}>
+    <g transform={`translate(${props.left} 0)`}> 
+      <PaddOne color="red" arrowLabel="Padd1"/>
+    </g> 
+    
+    <g transform={`translate(${props.left - 60} 6)`}> 
+      <PaddTwo color="blue" arrowLabel="Padd2"/>
+    </g>
 
-module.exports = ReactRedux.connect(mapStateToProps)(USPadd)
+    <g transform={`translate(${props.left - 48} 45)`}> 
+      <PaddThree color="black" arrowLabel="Padd3"/>
+    </g>
+
+    <g transform={`translate(${props.left - 85} 5)`}> 
+      <PaddFour color="orange" arrowLabel="Padd4"/>
+    </g>
+
+    <g transform={`translate(${props.left - 101}  6)`}> 
+      <PaddFive color="blue" arrowLabel="Padd5"/>
+    </g>
+	</g>*/
+)
+
+
+module.exports = USPadd
