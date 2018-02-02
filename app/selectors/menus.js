0ae2054fecb2f4ba23bc18c9bd3c5a3aec67920f
Spenser Jones <hello@spenserj.com>
2018-02-02 09:20:28 -0700

    NEBV-732 Activity menu options based on visualization support

--- app/selectors/menus.js
+++ app/selectors/menus.js
@@ -1,6 +1,12 @@
+import { createSelector } from 'reselect'
+
+import { selectedVisualization } from './visualizationSettings'
 import Constants from '../Constants'
 
-export const activityOptions = () => (['imports', 'exports', 'importsExports'])
+export const activityOptions = createSelector(
+  selectedVisualization,
+  vis => ((vis === 'crudeOil') ? ['exports'] : ['importsExports', 'imports', 'exports']),
+)
 
 export const arrangeByOptions = () => (['location', 'imports', 'exports'])
 
--- app/selectors/visualizationSettings.js
+++ app/selectors/visualizationSettings.js
@@ -1,7 +1,20 @@
 import { createSelector } from 'reselect'
 
-export const visualizationSettings = state =>
-  state.visualizationSettings[state.importExportVisualization]
+/* eslint-disable no-underscore-dangle */
+export const selectedVisualization = (state, props) =>
+  props._overrideVisualization || state.importExportVisualization
+export const arrangeByOverride = (_, props) => props._overrideArrangeBy
+export const amountOverride = (_, props) => props._overrideAmount
+export const activityGroupOverride = (_, props) => props._overrideActivityGroup
+/* eslint-enable no-underscore-dangle */
+
+const allSettings = state => state.visualizationSettings
+
+export const visualizationSettings = createSelector(
+  allSettings,
+  selectedVisualization,
+  (settings, vis) => settings[vis],
+)
 
 export const showImportsSelector = createSelector(
   visualizationSettings,
