bdd140c1e0a8db00b070e965c95ad44b7d95967d
Spenser Jones <hello@spenserj.com>
2018-02-15 17:51:04 -0700

    NEBV-836 Fix bug with poorly coded modernizr lib breaking IE11

--- app/components/SVGAnimation/SafeAnimation.jsx
+++ app/components/SVGAnimation/SafeAnimation.jsx
@@ -1,10 +1,34 @@
 /* global Modernizr */
 import React from 'react'
 import PropTypes from 'prop-types'
-import modernizrSVGTransformsTest from 'modernizr-svg-transforms'
 import DomReady from 'domready'
 
-DomReady(modernizrSVGTransformsTest)
+DomReady(() => {
+  Modernizr.addTest('svg-transforms', () => {
+    const vendorPrefixesCss = ['-moz-', '-ms-', '-webkit-', '']
+
+    // reduce vendor prefix array to create a cssText string.
+    const styleRules = vendorPrefixesCss.reduce((prev, curr) => `${prev} ${curr} transform: translateX(10px);`, '')
+
+    const body = document.querySelector('body')
+    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+    const path = document.createElementNS('http://www.w3.org/2000/svg', 'path')
+
+    // Append elements to dom
+    svg.appendChild(path)
+    svg.style.cssText = 'width: 100px; visibility: transparent;'
+    body.appendChild(svg)
+
+    // getBoundingClientRect to determine if transform applied successfully.
+    const initialBoundingRect = path.getBoundingClientRect()
+    path.style.cssText = styleRules
+    const newBoundingRect = path.getBoundingClientRect()
+
+    body.removeChild(svg)
+
+    return (initialBoundingRect.left !== newBoundingRect.left)
+  })
+})
 
 export const makeSafeAnimation = (WrappedComponent) => {
   const SafeAnimation = (props) => {
