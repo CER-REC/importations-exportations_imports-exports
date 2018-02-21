7f09e4f0b892e18571ee0c505834f5b0af46234b
Rahul Kamal Bhaskar <rahul.kamal.bhaskar@vizworx.com>
2018-02-21 12:45:38 -0700

    padd not visible in safari

--- app/components/Padds/USPadd.jsx
+++ app/components/Padds/USPadd.jsx
@@ -26,14 +26,6 @@ const USPadd = (props) => {
       country="us"
     />))
     return (<g transform={`scale(1.25) translate(-30 0)`}>
-      <filter id="paddOutline">
-        <feMorphology operator="dilate" in="SourceAlpha" radius="1.5"/>
-        <feColorMatrix result="recolored" type="matrix" values="-1 0 0 1 0, 0 -1 0 1 0, 0 0 -1 1 0, 0 0 0 1 0"/>
-        <feMerge>
-          <feMergeNode in="recolored"/>
-          <feMergeNode in="SourceGraphic"/>
-        </feMerge>
-      </filter>
       {padds.toArray()}
       {renderDetailSidebar(props)}
     </g>)
