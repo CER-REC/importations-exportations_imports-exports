71f94344c1df8bdf18a6331672a3819ed3fb9d0b
Spenser Jones <hello@spenserj.com>
2018-02-20 19:36:09 -0700

    NEBV-898 Adjusting timeline scale to always use full years when filtered

--- app/selectors/timeline.js
+++ app/selectors/timeline.js
@@ -1,7 +1,7 @@
 import { createSelector } from 'reselect'
 import { fromJS } from 'immutable'
 
-import { filterByHexSelector } from './data'
+import { activityGroupSelector, filterByHexSelector } from './data'
 import { visualizationContentPosition as visContentSize } from './viewport/'
 import { visualizationSettings, selectedVisualization } from './visualizationSettings'
 import Constants from '../Constants'
@@ -81,12 +81,35 @@ export const sortTimeline = createSelector(
   }),
 )
 
+export const timelineScaleBase = createSelector(
+  activityGroupSelector,
+  getAggregateKey,
+  selectedVisualization,
+  (points, aggregateKey, vizName) => fromJS(points.reduce((acc, next) => {
+    if (vizName === 'crudeOil' && aggregateKey === 'activity' && next.get('destination') === 'ca') {
+      return acc
+    }
+
+    const period = next.get('period')
+    if (!acc.period.min || period < acc.period.min) { acc.period.min = period }
+    if (!acc.period.max || period > acc.period.max) { acc.period.max = period }
+
+    const key = next.get(aggregateKey)
+    if (!key) { return acc }
+
+    if (!acc.values[period]) { acc.values[period] = {} }
+    acc.values[period][key] = (acc.values[period][key] || 0) + next.get('value', 0)
+
+    return acc
+  }, { period: {}, values: {} })),
+)
+
 export const timelineYearScaleCalculation = createSelector(
-  sortTimeline,
-  (points) => {
-    const years = mapToValue(points, 'year')
-    return { min: years.min() || 0, max: years.max() || 0 }
-  },
+  timelineScaleBase,
+  scaleBase => ({
+    min: parseInt(scaleBase.getIn(['period', 'min'], '2000Q1').substr(0, 4), 10),
+    max: parseInt(scaleBase.getIn(['period', 'max'], '2000Q4').substr(0, 4), 10),
+  }),
 )
 
 export const timelineScaleCalculation = createSelector(
@@ -142,9 +165,9 @@ export const timelinePositionCalculation = createSelector(
     const totalYears = (scale.x.max - scale.x.min)
     let offset = 0
     let lastPoint
-    let bars
     let barWidth
     const labels = []
+    let bars = points
 
     if (grouping === 'year') {
       const widthAfterPads = size.width
@@ -152,22 +175,24 @@ export const timelinePositionCalculation = createSelector(
         - ((totalYears * 4) * barPadding)
       barWidth = widthAfterPads / ((totalYears + 1) * 4)
 
-      bars = points.map((point) => {
-        if (!lastPoint || lastPoint.get('year') !== point.get('year')) {
-          if (lastPoint) { offset += groupPadding }
-          labels.push({
-            // Add two bars width, and then subtract half a bar for centering
-            offsetX: offset + (barWidth + barPadding) * 2 - (barWidth / 2),
-            label: point.get('year').toString().substr(-2),
-          })
+      for (let y = scale.x.min; y <= scale.x.max; y += 1) {
+        labels.push({
+          offsetX: (offset + ((barWidth + barPadding) * 2)) - (barWidth / 2),
+          label: y.toString().substr(-2),
+        })
+        for (let q = 1; q <= 4; q += 1) {
+          if (bars.has(`${y}Q${q}`)) {
+            bars = bars.setIn([`${y}Q${q}`, 'offsetX'], offset)
+          }
+          offset += barPadding + barWidth
         }
-        const newPoint = point.set('offsetX', offset)
-        offset += barPadding + barWidth
-        lastPoint = newPoint
-        return newPoint
-      })
+        offset += groupPadding
+      }
     } else {
-      barWidth = (size.width - (points.count() * barPadding)) / points.count()
+      const widthAfterPads = size.width
+        - (groupPadding * 3)
+        - ((totalYears * 4) * barPadding)
+      barWidth = widthAfterPads / ((totalYears + 1) * 4)
 
       const labelYears = [
         scale.x.min + 2,
@@ -177,38 +202,39 @@ export const timelinePositionCalculation = createSelector(
       ]
       let quarterStart = 0
 
-      bars = points.map((point) => {
-        if (lastPoint && lastPoint.get('quarter') !== point.get('quarter')) {
-          const dividerOffset = offset - barWidth / 2
+      for (let q = 1; q <= 4; q += 1) {
+        for (let y = scale.x.min; y <= scale.x.max; y += 1) {
+          if (labelYears.includes(y)) {
+            labels.push({
+              offsetX: offset,
+              label: y.toString().substr(-2),
+              key: `${y}Q${q}`,
+            })
+          }
+
+          if (bars.has(`${y}Q${q}`)) {
+            bars = bars.setIn([`${y}Q${q}`, 'offsetX'], offset)
+          }
+          offset += barPadding + barWidth
+        }
+
+        const dividerOffset = (offset - (barWidth / 2)) + (groupPadding / 2)
+        if (q !== 4) {
           labels.push({
             offsetX: dividerOffset,
             label: '|',
-            key: `divider-${point.get('quarter')}`,
-          })
-          labels.push({
-            offsetX: (dividerOffset - quarterStart) / 2 + quarterStart,
-            label: `Q${lastPoint.get('quarter')}`,
-            fontWeight: 'bold',
-          })
-          quarterStart = dividerOffset
-        }
-        if (labelYears.includes(point.get('year'))) {
-          labels.push({
-            offsetX: offset,
-            label: point.get('year').toString().substr(-2),
-            key: point.get('period'),
+            key: `divider-${q}`,
           })
         }
-        const newPoint = point.set('offsetX', offset)
-        offset += barPadding + barWidth
-        lastPoint = newPoint
-        return newPoint
-      })
+        labels.push({
+          offsetX: ((dividerOffset - quarterStart) / 2) + quarterStart,
+          label: `Q${q}`,
+          fontWeight: 'bold',
+        })
+        quarterStart = dividerOffset
 
-      labels.push({
-        offsetX: (offset - quarterStart) / 2 + quarterStart,
-        label: `Q${points.last().get('quarter')}`,
-      })
+        offset += groupPadding
+      }
     }
 
     return {
