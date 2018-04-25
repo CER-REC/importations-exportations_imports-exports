e109357bf25c826d36c3db0b626b8dd91b2dc873
Spenser Jones <hello@spenserj.com>
2018-04-25 14:56:00 -0600

    NEBV-989 Remove percentage from weighted average

--- app/components/DetailTotal.jsx
+++ app/components/DetailTotal.jsx
@@ -9,18 +9,24 @@ import { detailTotal } from '../selectors/details'
 import TR from '../TranslationTable'
 import { humanNumber } from '../utilities'
 
+const unitsWithoutPercentage = ['CAN$/MW.h', 'CN$/GJ']
+
 const DetailTotal = props => (
   <div className={`detailTotal ${props.type}`}>
-    <div className="detailText">
+    <div className={`detailText ${unitsWithoutPercentage.includes(props.amountUnit) ? '' : 'with-percentage'}`}>
       <span className="totalType">
         {TR.getIn(['importExportMenu', props.type, props.language])}
-      </span> {TR.getIn(['detailTotal', (props.average ? 'average' : 'total'), props.language])}&nbsp;
+      </span>
+      {props.average && !unitsWithoutPercentage.includes(props.amountUnit)
+        ? ''
+        : ` ${TR.getIn(['detailTotal', (props.average ? 'average' : 'total'), props.language])}`}&nbsp;
       {humanNumber(props.value, props.language)}&nbsp;
       {TR.getIn(['amounts', props.amountUnit, props.language])}&nbsp;
     </div>
-    <div className="detailBreakDownContainer">
-      <PercentageBar width={props.percentage} />
-    </div>
+    {unitsWithoutPercentage.includes(props.amountUnit)
+      ? null
+      : <div className="detailBreakDownContainer"><PercentageBar width={props.percentage} /></div>
+    }
   </div>
 )
 
