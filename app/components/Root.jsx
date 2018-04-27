c06457987854f6f8a15eb40b6791be58e233e90b
ccheuvizworx <30633884+ccheuvizworx@users.noreply.github.com>
2018-04-27 14:23:14 -0600

    NEBV-993 loading text

--- app/components/Root.jsx
+++ app/components/Root.jsx
@@ -6,28 +6,40 @@ import { hot } from 'react-hot-loader'
 
 import Workspace from './Workspace'
 
+import Tr from '../TranslationTable'
+import TrSelector from '../selectors/translate'
 import '../styles/Common.scss'
 
 const Root = ({ dataLoadingComplete, viewport }) => {
-  if (dataLoadingComplete) { return <div><Workspace /></div> }
+  //if (dataLoadingComplete) { return <div><Workspace /></div> }
 
   const loaderStyle = {
     height: viewport.get('y'),
     width: viewport.get('x'),
   }
+
   return <div style={loaderStyle}>
     <div className="loader" />
+      <p className="loadingVisualization">loading visualization</p>
+        <span className="loadingImports">Imports
+          <span className="loadingAnd"> and </span>
+          <span className="loadingExports">Exports</span>
+        </span>
+        <p className="loadingVisualizationTwo">of Energy Products to and from Canada</p>
     </div>
 }
 
 Root.propTypes = {
   dataLoadingComplete: PropTypes.bool.isRequired,
   viewport: PropTypes.instanceOf(Immutable.Map).isRequired,
+  language: PropTypes.string.isRequired,
 }
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state, props) => ({
   viewport: state.viewport,
   dataLoadingComplete: state.dataLoadingComplete,
+  Tr: TrSelector(state, props),
+  language: state.language,
 })
 
 export default hot(module)(connect(mapStateToProps)(Root))
