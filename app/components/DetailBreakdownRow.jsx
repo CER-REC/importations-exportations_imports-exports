692a5ea26f3b834a4686befb623361485641f51d
Spenser Jones <hello@spenserj.com>
2018-05-24 17:29:57 -0600

    Confidential in breakdown store in global state

--- app/components/DetailBreakdownRow.jsx
+++ app/components/DetailBreakdownRow.jsx
@@ -12,36 +12,35 @@ import { ConfidentialIconLogo } from './ConfidentialIcon'
 import MapLayoutGridConstant from '../MapLayoutGridConstant'
 import { arrangeBy } from '../selectors/data'
 import { visualizationSettings } from '../selectors/visualizationSettings'
+import { ExpandCollapseConfidentiality } from '../actions/confidentiality'
+import { handleInteraction } from '../utilities'
 
 import './DetailBreakDown.scss'
 
 const unitsWithoutPercentage = ['CAN$/MW.h', 'CN$/GJ']
 
-class PercentageBarConfidentialIcon extends React.Component {
-  constructor(props) {
-    super(props)
-    this.handleClick = this.handleClick.bind(this);
-    this.state = { active: false }
-  }
+const PercentageBarConfidentialIconUnconnected = props => (
+  <div
+    className="cofidentialTooltip"
+    {...handleInteraction(props.onClick, props.name)}
+  >
+    <svg className="confidentialIcon">
+      <g transform="translate(0 0) scale(0.8)">
+        <ConfidentialIconLogo />
+      </g>
+    </svg>
+    <span
+      className={`tooltiptext confidentialityText ${props.expanded ? 'show' : ''}`}
+    >
+      {`${props.confidential} / ${props.totalPoints} values confidential`}
+    </span>
+  </div>
+)
 
-  handleClick() {
-    this.setState({ active: !this.state.active })
-  }
-
-  render() {
-    const toggleClass = this.state.active ? 'show' : ''
-    return (
-      <div className="cofidentialTooltip" onClick={this.handleClick}>
-        <svg className="confidentialIcon">
-          <g transform="translate(0 0) scale(0.8)">
-            <ConfidentialIconLogo/>
-          </g>
-        </svg>
-        <span className={`tooltiptext confidentialityText ${toggleClass}`}>{`${this.props.confidential} / ${this.props.totalPoints} values confidential`}</span>
-      </div>
-    )
-  }
-}
+const PercentageBarConfidentialIcon = connect(
+  (state, props) => ({ expanded: state.openConfidentiality.contains(props.name) }),
+  { onClick: ExpandCollapseConfidentiality }
+)(PercentageBarConfidentialIconUnconnected)
 
 const DetailBreakdownRow = props => (
 
@@ -90,9 +89,9 @@ const DetailBreakdownRow = props => (
             ? null
             : (
               <PercentageBarConfidentialIcon
-                visualization={props.importExportVisualization}
                 confidential={props.confidential}
                 totalPoints={props.totalPoints}
+                name={`${props.importExportVisualization}-${props.name}`}
               />
             )}
         </td>
