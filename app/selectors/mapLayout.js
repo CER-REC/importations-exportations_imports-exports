695503dbd54692eca6c67746ee9efbe79197e81f
Rahul Kamal Bhaskar <rahul.kamal.bhaskar@vizworx.com>
2018-06-01 17:06:11 -0600

    natural gas liquids not showing butane and propane

--- app/selectors/mapLayout.js
+++ app/selectors/mapLayout.js
@@ -117,15 +117,15 @@ export const parseLocationData = createSelector(
         Object.entries(data.values).forEach(([key, value]) => {
           if (key.toLowerCase().includes('padd') || key.toLowerCase().includes('mexico')) return
           if (!calculatedValues[key]) {
-            calculatedValues[key] = {}
+            calculatedValues[key] = { imports:0 }
           }
           Object.entries(value).forEach(([subtype, amt]) => {
-            calculatedValues[key].imports = amt
+            calculatedValues[key].imports += amt
           })
         })
         data.values = calculatedValues
       } else {
-        data = calculateValueSum(records, ['originKey', 'destinationKey'], 'activity')
+        data = calculateValueAverage(records, ['originKey', 'destinationKey'], 'activity')
       }
     } else {
       data = calculateValueSum(records, ['originKey', 'destinationKey'], 'activity')
